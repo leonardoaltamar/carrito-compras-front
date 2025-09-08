e67bea21e00c9292ec4ce366527c27e22511ba3f
Leonardo altamar <leonardoaltamar12@gmail.com>
2025-09-08 17:37:00 -0500

    instalando redux

--- app/carrito-compras/page.tsx
+++ app/carrito-compras/page.tsx
@@ -1,6 +1,16 @@
+"use client"
 import { Button } from '@heroui/button'
-import React from 'react'
-
+import {
+    Drawer,
+    DrawerContent,
+    DrawerHeader,
+    DrawerBody,
+    DrawerFooter,
+} from "@heroui/drawer";
+import { useDisclosure } from './hooks/drawer';
+import { useState } from 'react';
+import { Input } from "@heroui/input";
+import { Divider } from "@heroui/divider";
 
 const productos = [
     {
@@ -13,7 +23,8 @@ const productos = [
             descripcion: 'tecnologica'
         },
         stock: 10,
-        inCar: false
+        inCar: false,
+        quantity: 0
     },
     {
         id: 2,
@@ -25,7 +36,8 @@ const productos = [
             descripcion: 'tecnologica'
         },
         stock: 2,
-        inCar: false
+        inCar: false,
+        quantity: 0
     },
     {
         id: 3,
@@ -37,50 +49,130 @@ const productos = [
             descripcion: 'tecnologica'
         },
         stock: 5,
-        inCar: false
+        inCar: false,
+        quantity: 0
     },
 ]
 
 
 const CarritoComprasScreen = () => {
+    const { isOpen, onOpen, onOpenChange } = useDisclosure();
+    const [productosEnCarrito, setProductosEnCarrito] = useState<any[]>([]);
+
+
+
+    const pushProductInShoppingCar = (producto: any) => {
+
+        if (productosEnCarrito.length > 0) {
+            const findedProduct = productosEnCarrito.find((product: any) => product.id === producto.id);
+            if (findedProduct) return;
+        }
+        setProductosEnCarrito([...productosEnCarrito, producto]);
+    }
+
+    const updatequantityProduct = (producto: any, operation: string) => {
+        /* console.log(operation)
+        switch (operation) {
+            case 'sum':
+                setProductosEnCarrito(prev => prev.map(product => producto.id === product.id ? { ...product, quantity: product.quantity + 1, stock: product.stock - 1 } : product))
+                console.log(productosEnCarrito)
+                break;
+
+            default:
+                setProductosEnCarrito(prev => prev.map(product => product.id === producto.id ? { ...product, quantity: product.quantity--, stock: product.stock++ } : product))
+                break;
+        } */
+    }
+
+
+
     return (
-        <div className='py-8 flex flex-col justify-center items-center'>            
-            <div className="flex items-center justify-between mb-4">
-                <h2 className="text-2xl font-bold">¿Qué productos deseas llevar?</h2>
-            </div>
+        <>
+            <div className='py-8 flex flex-col justify-center items-center'>
+                <div className="flex items-center justify-between mb-4">
+                    <h2 className="text-2xl font-bold">¿Qué productos deseas llevar?</h2>
+                </div>
 
 
-            <div className='content flex justify-center gap-4 flex-wrap'>
+                <div className='content flex justify-center gap-4 flex-wrap'>
 
-                {
-                    productos.map(product => (
-                        <div key={product.id} className='flex flex-col justify-between card-product border-gray-200 shadow-lg shadow-gray-500/10 border-1 rounded-[10px] py-8 min-w-[230px] px-4 relative'>
-                            <div className=' absolute top-2.5 right-2.5 flex gap-2'>
-                                <span className='bg-gray-200 px-2 rounded-full mb-2 inline-block text-sm'>{product.stock}</span> 
-                            </div>                    
-                            <img className='w-[150px] mb-4 mx-auto' src={product.img} alt="" />
-                            <div>
-                                <h2 className='font-bold mb-2 text-center'>{product.nombre}</h2>
-                                <h2 className='text-sm mb-4 text-center'>${product.precio}</h2>
+                    {
+                        productos.map(product => (
+                            <div key={product.id} className='flex flex-col justify-between card-product border-gray-200 shadow-lg shadow-gray-500/10 border-1 rounded-[10px] py-8 min-w-[230px] px-4 relative'>
+                                <div className=' absolute top-2.5 right-2.5 flex gap-2'>
+                                    <span className='bg-gray-200 px-2 rounded-full mb-2 inline-block text-sm'>{product.stock}</span>
+                                </div>
+                                <img className='w-[150px] mb-4 mx-auto' src={product.img} alt="" />
+                                <div>
+                                    <h2 className='font-bold mb-2 text-center'>{product.nombre}</h2>
+                                    <h2 className='text-sm mb-4 text-center'>${product.precio}</h2>
+                                </div>
+                                <Button color="primary" variant="bordered" fullWidth className='rounded-full' onPress={() => { pushProductInShoppingCar(product) }}>
+                                    Ir al carrito
+                                </Button>
                             </div>
-                            <Button color="primary" variant="bordered" fullWidth className='rounded-full'>
-                                Ir al carrito
-                            </Button>
-                        </div>
-                    ))
-                }
+                        ))
+                    }
 
+                </div>
+
+                <div className="flex gap-2.5 fixed bottom-0 left-[50%] transform-[translateX(-50%)] p-3 rounded-t-2xl border-1 border-gray-200 w-full max-w-[360px] shadow-lg shadow-gray-500/50">
+                    <Button className='rounded-full' color="danger" variant="bordered" fullWidth>
+                        Atrás
+                    </Button>
+                    <Button className='rounded-full' color="primary" variant="bordered" fullWidth onPress={onOpen}>
+                        Ver carrito {productosEnCarrito.length > 0 ? `(${productosEnCarrito.length})` : ''}
+                    </Button>
+                </div>
             </div>
 
-            <div className="flex gap-2.5 fixed bottom-0 left-[50%] transform-[translateX(-50%)] p-3 rounded-t-2xl border-1 border-gray-200 w-full max-w-[360px] shadow-lg shadow-gray-500/50">
-                <Button className='rounded-full' color="danger" variant="bordered" fullWidth>
-                    Atrás
-                </Button>
-                <Button className='rounded-full' color="primary" variant="bordered" fullWidth>
-                    Ver carrito {/* (1) */}
-                </Button>
-            </div>
-        </div>
+            <Drawer isOpen={isOpen} onOpenChange={onOpenChange}>
+                <DrawerContent>
+                    {(onClose) => (
+                        <>
+                            <DrawerHeader className="flex flex-col gap-1">Carrito de compras</DrawerHeader>
+                            <DrawerBody>
+                                {
+                                    productosEnCarrito.length === 0
+                                        ? <p>No hay productos en el carrito.</p>
+                                        : productosEnCarrito.map(productEnCarrito => (
+                                            <div>
+                                                <div key={productEnCarrito.id} className='flex gap-4 items-center'>
+                                                    <div>
+                                                        <img className='w-[150px] mb-4 mx-auto' src={productEnCarrito.img} alt="" />
+                                                    </div>
+                                                    <div className='flex flex-col'>
+                                                        <h2 className='font-bold mb-2 text-center'>{productEnCarrito.nombre}</h2>
+                                                        <h2 className='text-sm text-center mb-4'>${productEnCarrito.precio}</h2>
+                                                        <div className='flex items-center gap-2'>
+                                                            <Button className='rounded-full w-2 text-2xl' color="danger" variant="light" onPress={() => updatequantityProduct(productEnCarrito, 'res')}>
+                                                                -
+                                                            </Button>
+                                                            <span>{productEnCarrito.quantity}</span>
+                                                            <Button className='rounded-full w-2 text-2xl' color="primary" variant="light" onPress={() => updatequantityProduct(productEnCarrito, 'sum')}>
+                                                                +
+                                                            </Button>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <Divider className="my-4" />
+                                            </div>
+                                        ))
+                                }
+                            </DrawerBody>
+                            <DrawerFooter>
+                                <Button className='rounded-full' color="danger" variant="bordered" onPress={onClose}>
+                                    cerrar
+                                </Button>
+                                <Button className='rounded-full' color="primary" variant="bordered" onPress={onClose}>
+                                    Comprar
+                                </Button>
+                            </DrawerFooter>
+                        </>
+                    )}
+                </DrawerContent>
+            </Drawer>
+        </>
     )
 }
 

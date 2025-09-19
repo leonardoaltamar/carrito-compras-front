d84c0a0b1c2ff9c729706c31faf94c850181a532
Leonardo altamar <leonardoaltamar12@gmail.com>
2025-09-19 10:01:33 -0500

    subiendo cambios

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
+import { redirect } from "next/navigation";
+
 export default function Home() {
-  return (
-    <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-      asdasdasdasad
-    </section>
-  );
+  redirect("/login");
 }
